274a013ee76d6fabf13d1977456f1c6583f889dc
Steven-Cadena <steven.cadena.giler@gmail.com>
2022-05-09 10:14:25 +0200

    Add project files.

---
+++ MvcDynamoAWS/Models/Coche.cs
@@ -0,0 +1,22 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcDynamoAWS.Models
+{
+    [DynamoDBTable("coches")]
+    public class Coche
+    {
+        [DynamoDBHashKey]
+        [DynamoDBProperty("idcoche")]
+        public int IdCoche { get; set; }
+        [DynamoDBProperty("marca")]
+        public string Marca { get; set; }
+        [DynamoDBProperty("modelo")]
+        public string Modelo { get; set; }
+        [DynamoDBProperty("velocidad")]
+        public int Velocidad { get; set; }
+    }
+}
---
+++ MvcDynamoAWS/Services/ServiceDynamoDB.cs
@@ -0,0 +1,40 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using MvcDynamoAWS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcDynamoAWS.Services
+{
+    public class ServiceDynamoDB
+    {
+        private DynamoDBContext context;
+        public ServiceDynamoDB() 
+        {
+            //A PARTIR DE UN CLIENTE DYNAMODB SE CREA UN CONTEXT
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+            this.context = new DynamoDBContext(client);
+        }
+        //metodo asincrono
+        public async Task CreateCocheAsync(Coche car) 
+        {
+            await this.context.SaveAsync<Coche>(car);
+        }
+
+        public async Task DeleteCocheAsync(int idCoche) 
+        {
+            await this.context.DeleteAsync<Coche>(idCoche);
+        }
+
+        public async Task<Coche> FindCocheAsync(int idCoche) 
+        {
+            //SI ESTAMOS BUSCANDO POR SU PARTITION KEY(PRIMARY KEY)
+            //TENEMOS UN METODO PARA CARGAR SU BUSQUEDA 
+            return await this.context.LoadAsync<Coche>(idCoche);
+        }
+
+
+    }
+}
